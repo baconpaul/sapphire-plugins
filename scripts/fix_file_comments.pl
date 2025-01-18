97fd1a8c6013cb2456bd433b30a5fdcbbc9e45d5
Paul Walker <paul@pwjw.com>
2025-01-18 10:19:18 -0500

    Correct the header

--- scripts/fix_file_comments.pl
+++ scripts/fix_file_comments.pl
@@ -22,8 +22,11 @@ sub findfiles
  *
  * Bringing the magical world of CosineKitty's sapphire plugins for rack to your DAW
  *
- * Copyright 2024-2025, Don Cross, Paul Walker and Various authors, as described in the github
- * transaction log.
+ * Copyright 2024-2025, Don Cross, Paul Walker, Morgon Kanter and other authors, as
+ * described in the github transaction log.
+ *
+ * This project is distributed under the Gnu General Public License, version 3.0 or later.
+ * You can find the LICENSE file at the address below.
  *
  * The source code and license are at https://github.com/baconpaul/sapphire-plugins
  */
